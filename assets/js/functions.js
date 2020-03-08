f72f719b3fd6d2e4c0926f7b5b68be01c96d576c
Alexis Vilaça <cpm.alexisvilaca@gmail.com>
2020-03-08 11:36:52 +0000

    Website v3.0.0

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,3 +9,16 @@ $(document).ready(function(){
 $('.hamburgerMenu').click( function() {
     $(".hamburgerNavigation").toggleClass("show");
 } );
+
+
+
+$(document).ready(function(){
+	$('.hero-hamburgerMenu').click(function(){
+		$(this).toggleClass('hero-open');
+	});
+});
+
+
+$('.hero-hamburgerMenu').click( function() {
+    $(".hero-hamburgerNavigation").toggleClass("hero-show");
+} );
